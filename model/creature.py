e85da9ce6a8e2e758d98c546d1939c14abaf75f9
alexey.nilov <alexey.nilov@ringcentral.com>
2024-09-22 20:26:34 +0200

    spiking

--- model/creature.py
+++ model/creature.py
@@ -33,7 +33,7 @@ class Wolf(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('image/wolf.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (80, 120))
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.center = (500, 400)
 
